9b8db723455d3fae05b13c935bcda3ac7d0dbf34
ngthanhvinh <ngthanhvinh1996@gmail.com>
2026-03-06 00:03:11 +0700

    Add method sharemem camera

--- ros2_ws/src/robot_camera/include/robot_camera/robot_camera_node.hpp
+++ ros2_ws/src/robot_camera/include/robot_camera/robot_camera_node.hpp
@@ -13,17 +13,25 @@
 
 namespace robot_cam
 {
-
-typedef struct Publisher_info {
+typeded struct Publisher_info_base {
     sensor_msgs::msg::CameraInfo::UniquePtr camera_calibration_info_;
     sensor_msgs::msg::CameraInfo::UniquePtr camera_calibration_info2_;
     rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr info_pub_ = nullptr;
     rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr info_pub2_ = nullptr;
     std::chrono::time_point<std::chrono::system_clock> time_start_;
+}
+
+typedef struct Publisher_info : Publisher_info_base {
     sensor_msgs::msg::Image::UniquePtr img_;
     std::string topic_type;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub_ = nullptr;
 } Publisher_info_st;
+
+typedef struct Publisher_hbmem_info : Publisher_info_base {
+    int mSendIdx;
+    rclcpp::Publisher<hbm_img_msgs::msg::HbmMsg1080P>::SharedPtr publisher_hbmem_;
+    std::string topic_type;
+}
 
 class RobotCameraNode : public rclcpp::Node {
 public:
@@ -31,8 +39,10 @@ public:
     ~RobotCameraNode();
     void init();
     void update(Publisher_info_st* pub_info);
+    void hbmemUpdate(Publisher_hbmem_info_st* pub_info);
 private:
     void init_publisher(Publisher_info_st &Pub_info, std::string topic, std::string topic_type, std::string frame_id);
+    void init_publisher_hbmem(Publisher_hbmem_info_st &Pub_info, std::string topic, std::string topic_type);
     std::shared_ptr<RobotCamera> robotCam_ptr_;
     std::vector<std::shared_ptr<std::thread>> timer_;
     rclcpp::TimerBase::SharedPtr timer_tmp_;
